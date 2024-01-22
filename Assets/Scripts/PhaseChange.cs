56eb475cbe99db554c6e97764624b8608374443c
Kouiyere <arthur.de.bailliencourt@gmail.com>
2024-01-22 11:54:34 +0100

    State check fonctional

--- Assets/Scripts/PhaseChange.cs
+++ Assets/Scripts/PhaseChange.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PhaseChange : MonoBehaviour
 {
     [Range(0, 6)]
-    public int state;
+    private int state;
     //0 = Solide
     //1 = Liquide
     //2 = Gazeux
@@ -15,9 +15,9 @@ public class PhaseChange : MonoBehaviour
     //6 = Sol/Liq/gaz
 
     private Vector2 SLPoint = new Vector2(180, 10);
-    private Vector2 LGPoint = new Vector2(600, 9) ;
-    private Vector2 SGPoint = new Vector2(120, 1);
-    private Vector2 Triplepoint = new Vector2(180, 2);
+    private Vector2 LGPoint = new Vector2(600, 8) ;
+    private Vector2 SGPoint = new Vector2(120, 0);
+    private Vector2 Triplepoint = new Vector2(180, 1);
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -25,45 +25,139 @@ public class PhaseChange : MonoBehaviour
         float pres = collision.gameObject.GetComponent<RoomParameter>().pressure;
         Vector2 param = new Vector2(temp, pres);
         StateCheck(param);
+        Debug.Log(state);
     }
 
     void StateCheck(Vector2 param)
     {
-        if (param == Triplepoint)
+        if (IsGaz(param) && IsLiquid(param) && IsSolid(param))
         {
             state = 6;
         }
-        else if (param == SGPoint)
+        else if (IsGaz(param) && IsSolid(param))
         {
             state = 5;
         }
-        else if (param == LGPoint)
+        else if (IsGaz(param) && IsLiquid(param))
         {
             state = 4;
         }
-        else if (param == SGPoint)
+        else if (IsLiquid(param) && IsSolid(param))
         {
             state = 3;
         }
         else if (IsGaz(param))
         {
-
+            state = 2;
+        }
+        else if (IsLiquid(param))
+        {
+            state = 1;
+        }
+        else if (IsSolid(param))
+        {
+            state = 0;
         }
     }
 
     bool IsGaz(Vector2 param)
     {
-        if (param.x < SGPoint.x)
+        if (param.x <= Triplepoint.x)
         {
-            return false;
+            if (param.y <= SGFunction(param.x))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
-        else if (param.y > LGPoint.y)
+        else if (param.x >= Triplepoint.x)
         {
-            return false;
+            if (param.y <= LGFunction(param.x))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
-            return true;
+            return false;
         }
+    }
+
+    bool IsLiquid(Vector2 param)
+    {
+        if (param.x >= Triplepoint.x)
+        {
+            if (param.y >= LGFunction(param.x))
+            {
+                return true;
+            }
+            else if (param.y <= SLFunction(param.x))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool IsSolid(Vector2 param)
+    {
+        if (param.x <= Triplepoint.x)
+        {
+            if (param.y >= SGFunction(param.x))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (param.x >= Triplepoint.x)
+        {
+            if (param.y >= SLFunction(param.x))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private float SGFunction(float x)
+    {
+        float y = ((SGPoint.y - Triplepoint.y) / (SGPoint.x - Triplepoint.x)) * x + (SGPoint.y - SGPoint.x * (((SGPoint.y - Triplepoint.y) / (SGPoint.x - Triplepoint.x))));
+        return y;
+    }
+
+    private float LGFunction(float x)
+    {
+        float y = ((LGPoint.y - Triplepoint.y) / (LGPoint.x - Triplepoint.x)) * x + (LGPoint.y - LGPoint.x * (((LGPoint.y - Triplepoint.y) / (LGPoint.x - Triplepoint.x))));
+        return y;
+    }
+
+    private float SLFunction(float x)
+    {
+        float y = ((SLPoint.y - Triplepoint.y) / (SLPoint.x - Triplepoint.x)) * x + (SLPoint.y - SLPoint.x * (((SLPoint.y - Triplepoint.y) / (SLPoint.x - Triplepoint.x))));
+        return y;
     }
 }
